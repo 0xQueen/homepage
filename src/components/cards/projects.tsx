21947d1de624cb9e031d55edfa4da1dd07df4aa9
Queen <156551265+0xQueen@users.noreply.github.com>
2024-03-05 22:59:41 +0300

    Update projects.tsx
    made inline instead outline

--- src/components/cards/projects.tsx
+++ src/components/cards/projects.tsx
@@ -17,22 +17,22 @@ const Card: React.FC<CardProps> = ({ name, description, repo_link }) => {
       style={{ textDecoration: "none" }}
     >
       <div
-        className="bg-gray-800 text-white rounded-lg p-4 md:p-6 shadow-md transition duration-300 hover:bg-[#9a397c] hover:border-[#9a397c] hover:text-[#9a397c] hover:border-2 mt-10 sm:mt-0"
+        className="bg-gray-800 text-white rounded-lg p-4 md:p-6 shadow-md transition duration-300 mt-10 sm:mt-0 cursor-pointer relative"
         style={{
           backgroundColor: "#382434",
           boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-          borderColor: "#9a397c",
           maxWidth: "90%",
           width: "100%",
           margin: "auto",
         }}
       >
+        <div className="absolute inset-0 border border-transparent hover:border-[#9a397c] rounded-lg transition duration-300"></div>
         <h3 className="text-gray-300 text-left font-bold mb-2">{name}</h3>
         <p className="text-gray-300 mb-4 text-left">{description}</p>
         <div className="flex items-center justify-between">
           <span className="flex items-center text-gray-300">
             <FaLink className="mr-2" />
-            <span>github.com</span>
+            <span className="hover:text-[#9a397c]">github.com</span>
           </span>
         </div>
       </div>
