d778e3dfa26444cfafd3096a41f7a40750c8eba3
Queen <156551265+0xQueen@users.noreply.github.com>
2024-03-18 04:27:41 +0100

    Add files via upload
    Added more sources, thanks to PC_FOCUS.

--- src/components/links/trusted.tsx
+++ src/components/links/trusted.tsx
@@ -1,24 +1,67 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const TrustedLinks: React.FC = () => {
-  const trustedLinks = [
-    "1337x.to",
-    "thepiratebay.org",
+  const [trustedLinks, setTrustedLinks] = useState<string[]>([
+    /* "1337x.to", BYE BYE!*/
+    /* "thepiratebay.org", BYE BYE!*/
     "dodi-repacks.site",
     "fitgirl-repacks.site",
     "gamedrive.org",
     "steamrip.com",
     "repack-mechanics.com",
     "gog-games.to",
+  ]);
+
+  const [newlyAddedSources, setNewlyAddedSources] = useState<string[]>([]);
+  const newSources = [
+    "gamebounty.world",
+    "dodi-repacks.download",
+    "elamigos.site",
+    "kaoskrew.org",
+    "cs.rin.ru",
+    "rutracker.org",
+    "online-fix.me",
+    "torrentgalaxy.to",
+    "torrentleech.com",
+    "rlsbb.ru",
+    "dlfox.com",
+    "ovagames.com",
+    "rihnogames.com",
+    "campucalitv.com",
+    "fmoviesz.to",
+    "hurawatch.bz",
   ];
+
+  useEffect(() => {
+    const updatedLinks = [...trustedLinks];
+    const updatedNewSources = [...newlyAddedSources];
+
+    newSources.forEach((source) => {
+      if (
+        !updatedLinks.includes(source) &&
+        !updatedNewSources.includes(source)
+      ) {
+        updatedLinks.push(source);
+        updatedNewSources.push(source);
+      }
+    });
+
+    setTrustedLinks(updatedLinks);
+    setNewlyAddedSources(updatedNewSources);
+  }, [trustedLinks]);
 
   return (
     <div className="bg-2e1a27 rounded-lg p-2">
       <ul className="list-disc pl-4 text-left">
         {trustedLinks.map((link, index) => (
           <li key={index} className="text-gray-300">
-            {link}
+            {link}{" "}
+            {newlyAddedSources.includes(link) && (
+              <span className="new bg-purple-600 text-white text-xs ml-0.2 px-2 py-0.9 rounded-full">
+                NEW!
+              </span>
+            )}
           </li>
         ))}
       </ul>
--- src/components/links/untrusted.tsx
+++ src/components/links/untrusted.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const UntrustedLinks: React.FC = () => {
-  const untrustedLinks = [
+const untrustedLinks: React.FC = () => {
+  const [untrustedLinks, setuntrustedLinks] = useState<string[]>([
     "1377x.to",
     "IGG-Games.com",
     "steamunlocked.com",
@@ -11,14 +11,50 @@ const UntrustedLinks: React.FC = () => {
     "skidrowreloaded.com",
     "skidrowempress.com",
     "skidrowcodex.net",
+  ]);
+
+  const [newlyAddedSources, setNewlyAddedSources] = useState<string[]>([]);
+  const newSources = [
+    "1337x.to",
+    "thepiratebay.org",
+    "kickasstorrents.com",
+    "oceanofgames.com",
+    "*EVERY* RARBG",
+    "getintopc.com",
+    "yasdl.com",
+    "torrentlite.com",
+    "crackedfully.com",
   ];
+
+  useEffect(() => {
+    const updatedLinks = [...untrustedLinks];
+    const updatedNewSources = [...newlyAddedSources];
+
+    newSources.forEach((source) => {
+      if (
+        !updatedLinks.includes(source) &&
+        !updatedNewSources.includes(source)
+      ) {
+        updatedLinks.push(source);
+        updatedNewSources.push(source);
+      }
+    });
+
+    setuntrustedLinks(updatedLinks);
+    setNewlyAddedSources(updatedNewSources);
+  }, [untrustedLinks]);
 
   return (
     <div className="bg-2e1a27 rounded-lg p-2">
       <ul className="list-disc pl-4 text-left">
         {untrustedLinks.map((link, index) => (
           <li key={index} className="text-gray-300">
-            {link}
+            {link}{" "}
+            {newlyAddedSources.includes(link) && (
+              <span className="new bg-purple-600 text-white text-xs ml-0.2 px-2 py-0.9 rounded-full">
+                NEW!
+              </span>
+            )}
           </li>
         ))}
       </ul>
@@ -26,4 +62,4 @@ const UntrustedLinks: React.FC = () => {
   );
 };
 
-export default UntrustedLinks;
+export default untrustedLinks;
